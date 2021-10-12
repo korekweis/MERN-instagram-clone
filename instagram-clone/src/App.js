8a09afe89fa4cb00f046e9c3ecab2b449b0e0dd1
korekweis <korekweis@gmail.com>
2021-10-11 22:44:22 -0700

    material UI and setting up modal

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -3,26 +3,41 @@ import './App.css';
 import Post from './Post';
 import { doc, collection, onSnapshot, getDocs } from "firebase/firestore";
 import { db, auth, storage } from './firebase';
+import Modal from '@mui/material/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@mui/material/Button';
+
+
+
+function getModalStyle() { 
+  const top = 50;
+  const left = 50;
+
+  return { 
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  }
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: { 
+    position: "absolute",
+    width: 400, 
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5], 
+    padding: theme.spacing(2, 4, 3),
+  }, 
+}));
 
 function App() {
-  const [posts, setPosts] = useState([
-    // {
-    //   username: "SundaeKids",
-    //   avatar: "https://pbs.twimg.com/profile_images/881072525744066561/Huu_nrpb_400x400.jpg",
-    //   imgsrc: "https://i.pinimg.com/originals/96/24/b7/9624b733da1adc91448ba4ab552c74d3.jpg", 
-    //   caption: "My everyday mood!!"
-    // }, {
-    //   username: "The Economist",
-    //   avatar: "https://mir-s3-cdn-cf.behance.net/project_modules/disp/1e154b10868229.5631a16e7ce8f.jpg",
-    //   imgsrc: "https://cdn.shopify.com/s/files/1/0591/1981/products/20200328_cna1280_0.jpg?v=1585335486",
-    //   caption: "Is the government telling the truth?"
-    // }, {
-    //   username: "Vogue",
-    //   avatar: "https://prh.org/wp-content/uploads/2017/10/vogue-logos.jpg",
-    //   imgsrc: "https://assets.vogue.com/photos/5877208c765c6c7b400ee583/master/w_1600%2Cc_limit/11-vogue-covers_113725704600.jpg",
-    //   caption: "Nothing can beat the classic covers from the 1940s!"
-    // }
-  ]);
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle); 
+  
+  const [posts, setPosts] = useState([]);
+  //checking if the modal is open or not 
+  const [open, setOpen] = useState(false);
 
   const fetchPost = async() => {
     const posts = collection(db, 'posts');
@@ -46,6 +61,17 @@ function App() {
 
   return (
     <div className="App">
+
+      <Modal
+        // copied from material UI 
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}> 
+          <h2>I AM A MODAL</h2>
+        </div>
+      </Modal>
+
       {/* header */}
       <div className="app_header">
         <img className="app_headerImg"
@@ -53,6 +79,9 @@ function App() {
         alt="" >
         </img>
       </div>
+
+      <Button></Button>
+
       <h1>HELLO</h1>
       {/* Bunch of posts: 
         - post 
